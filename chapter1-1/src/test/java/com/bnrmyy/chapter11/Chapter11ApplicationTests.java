16dad258076e8b901088c3fa95c257f0f5e8c9b1
coderyangjie <327409251@qq.com>
2021-04-19 14:36:32 +0800

    测试类编写

--- chapter1-1/src/test/java/com/bnrmyy/chapter11/Chapter11ApplicationTests.java
+++ chapter1-1/src/test/java/com/bnrmyy/chapter11/Chapter11ApplicationTests.java
@@ -1,13 +1,36 @@
 package com.bnrmyy.chapter11;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class Chapter11ApplicationTests {
 
+    private MockMvc mvc;
+
+
+    @Before
+    public void setUp() throws Exception{
+        /*使用MockServletContext来构建一个空的WebApplicationContext，
+        这样我们创建的HelloController就可以在@Before函数中创建并传递到MockMvcBuilders.standaloneSetup()函数中。*/
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
     @Test
-    void contextLoads() {
+    public void getHello() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World")));
     }
 
 }
