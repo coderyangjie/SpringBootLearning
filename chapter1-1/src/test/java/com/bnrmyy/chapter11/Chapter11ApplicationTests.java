aa19fad725f05768761b8c8fc1ec503af697d934
coderyangjie <327409251@qq.com>
2021-04-19 12:28:43 +0800

    首次提交

---
+++ chapter1-1/src/test/java/com/bnrmyy/chapter11/Chapter11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnrmyy.chapter11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
